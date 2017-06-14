5fa4592ea657905ffd7f425f917de0787c0647ba
shikharkapoor <shikhar.kapoor@flipkart.com>
2017-06-14 23:03:28 +0530

    Adds service worker caching

--- sw.js
+++ sw.js
@@ -1,17 +1,39 @@
 var log = console.log.bind(console);
 var err = console.error.bind(console);
 
+var CACHE_NAME = 'pwa-demo-cache-v1';
+var urlsToCache = [
+	'/',
+	'/public/css/styles.css',
+	'/public/js/app.js',
+	new Request('https://www.reddit.com/.json')
+];
+
 self.addEventListener('install',function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
 	log('Service Worker: Installed');
-	//event.waitUntil(
-	//	fetchStuffAndInitDatabases()
-	//);
 });
 
 self.addEventListener('activate',function() {
 	log('Service Worker: Active');
 });
 
-self.addEventListener('fetch',function() {
+self.addEventListener('fetch',function(event) {
+	event.respondWith(
+		caches.open(CACHE_NAME).then(function(cache) {
+			return cache.match(event.request).then(function(response) {
+				var fetchPromise = fetch(event.request).then(function(networkResponse) {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+				return response || fetchPromise;
+			})
+		})
+	);
 	log('Service Worker: Fetch');
 });
