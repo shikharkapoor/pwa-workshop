8b7decc6d6c38597cdfdc10674b0eb9fcb1ce9db
shikharkapoor <shikhar.kapoor@flipkart.com>
2017-06-14 20:14:59 +0530

    Adds app shell

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var hbs = require('express-handlebars');
 
 var port = process.env.PORT || 3000;
 
+
+app.use(express.static(path.join(__dirname)));
+
+app.set('views', path.join(__dirname, '/public'));
+app.set('view engine', '.hbs');
+app.engine('.hbs', hbs({
+	'extname': '.hbs'
+}));
+
+var inlineStyles = fs.readFileSync(path.resolve(__dirname) + '/public/styles/shell-styles.css', 'utf8');
 app.get('/', function (req, res) {
-	res.send('Hello World!')
+	var templateVars = {
+		'inlineStyles': inlineStyles
+	};
+	res.render('index', templateVars);
 });
 
 app.listen(port, function () {
