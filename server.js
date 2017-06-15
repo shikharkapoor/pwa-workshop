75bfbdf7429ac4a3d0c1ec49e7ca34e8bf3d2486
shikharkapoor <shikhar.kapoor@flipkart.com>
2017-06-15 09:56:42 +0530

    Removes handlebars from the server

--- server.js
+++ server.js
@@ -2,25 +2,14 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
-var hbs = require('express-handlebars');
 
 var port = process.env.PORT || 3000;
 
 
 app.use(express.static(path.join(__dirname)));
 
-app.set('views', path.join(__dirname, '/public'));
-app.set('view engine', '.hbs');
-app.engine('.hbs', hbs({
-	'extname': '.hbs'
-}));
-
-var inlineStyles = fs.readFileSync(path.resolve(__dirname) + '/public/styles/shell-styles.css', 'utf8');
 app.get('/', function (req, res) {
-	var templateVars = {
-		'inlineStyles': inlineStyles
-	};
-	res.render('index', templateVars);
+	res.type('html').sendFile(path.join(__dirname, '/public/') + 'index.html');
 });
 
 app.listen(port, function () {
